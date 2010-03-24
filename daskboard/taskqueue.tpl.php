716f6445d49b182359338c7962dcfb8f6950834e
Thomas Fini Hansen <xen@scalpel.peytz.loc>
2010-03-24 22:36:03 +0100

    Experiments converted to proper theming. That took longer than expected.

---
+++ daskboard/taskqueue.tpl.php
@@ -0,0 +1,8 @@
+<div class="<?php print $classes; ?>">
+<?php if ($title) : ?>
+   <h1><?php print $title; ?></h1>
+<?php endif; ?>
+<div class=inner>
+   <?php print render($items); ?>
+</div>
+</div>
---
+++ task/node--taskcard.tpl.php
@@ -0,0 +1,17 @@
+<?php
+// $Id: node.tpl.php,v 1.31 2010/01/04 03:57:19 webchick Exp $
+
+/**
+ * @file
+ * Special rendering of nodes.
+ */
+?>
+<div id="node-<?php print $node->nid; ?>" class="taskcard <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $node_title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+</div>
